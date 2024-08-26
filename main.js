67abbe5069b8c017be389faddfa03f486efc93e6
Andrew Quartermain <quartermain.andrew@gmail.com>
2024-08-27 07:10:16 +0800

    Add enemies and *very* random level generation

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ onResize();
 const colors = ["#6df7c1", "#11adc1", "#606c81", "#393457", "#1e8875", "#5bb361", "#a1e55a", "#f7e476", "#f99252", "#cb4d68", "#6a3771", "#c92464", "#f48cb6", "#f7b69e", "#9b9c82"
 ]
 
-const DIRECTIONS = [[0,0], [-1,0], [1,0], [0,-1], [0,1]];
+const DIRECTIONS = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]];
 
+window.gameRunning = false;
 
 function loadGame() {
     createGrid();
     window.level = 1;
     window.room = 1;
     window.moves = 0;
+    window.movesInRoom = 0;
     createPlayer();
     setupGame();
-    
+
 }
 
 function setupGame() {
@@ -27,7 +29,8 @@ function setupGame() {
     player.y = 12;
     render();
     window.addEventListener("keydown", onKeyDown);
-
+    gameRunning = true;
+    movesInRoom = 0;
 }
 
 function createPlayer() {
@@ -47,11 +50,16 @@ function createMap() {
             gameMap[i][j] = 0;
         }
     }
-    const tiles = [1];
+    const tiles = [];
+
+    let tileQuantity = Math.max(1, Math.floor(Math.random() * room));
+    let minValue = room * level < 2 ? 1 : -Math.max(1, Math.floor(Math.random() * room));
+    let maxValue = Math.max(1, Math.floor(Math.random() * room));
+
     // const tiles = [1,1,1,1,1,1,1,1,1,1,1,1,1];
-    for (let i = 1; i <= 13; i++) {
-        for (let j = -level + i - 1; j <= level - i + 1; j++) {
-            tiles.push(Math.max(-room + 1, Math.min(j, 13)));
+    for (let i = 0; i < tileQuantity; i++) {
+        for (let j = minValue; j <= maxValue; j++) {
+            tiles.push(j);
         }
     }
     if (tiles.length > (13 * 13) - 2) {
@@ -73,13 +81,23 @@ function createMap() {
 }
 
 function spawnMobs() {
+    let quantity = level * room === 1 ? 0 : (level === 1 || level * room < 6) ? Math.floor(Math.random() * 2) : Math.floor(Math.random() * room);
+    let minLevel = Math.max(1, Math.floor(Math.random() * level - 6));
+    let maxLevel = level === 1 ? 1 : Math.random() < (level / 13) ? 13 : Math.floor(Math.random() * level);
+
+    for (let i = 0; i < quantity; i++) {
+        spawnMob(minLevel, maxLevel);
+    }
+}
+
+function spawnMob(minLevel=1, maxLevel=level) {
     const x = Math.floor(Math.random() * 11) + 1;
     const y = Math.floor(Math.random() * 6) + 1;
-    const score = Math.floor(Math.random() * 12 + 1);
+    const score = Math.floor(Math.random() * (maxLevel - minLevel)) + minLevel;
     gameMap[x][y] = 0;
     element = document.createElement("div");
     element.classList.add('mob');
-    mobs.push({x, y, score, element});
+    mobs.push({ x, y, score, element });
 }
 
 function render() {
@@ -101,7 +119,7 @@ function render() {
                 element.textContent = "X";
                 element.style.color = "#404040";
             }
-            
+
             element.style.color = num <= 0 ? colors[-num] : "#000";
             element.style.outlineColor = num <= 0 ? colors[-num] : colors[num];
             element.style.backgroundColor = num <= 0 ? "#000" : colors[num] + "80";
@@ -121,7 +139,8 @@ function render() {
         mob.element.style.color = mob.score > player.score ? "#f00" : mob.score < player.score ? "#0f0" : "#444";
     });
 
-    levelText.textContent = `${level}:${room}`;  
+    movesText.textContent = `Moves: ${moves}`;
+    levelText.textContent = `${level}:${room}`;
     levelText.style.color = colors[level];
 }
 
@@ -146,11 +165,11 @@ function onResize() {
 
 function handleInput(dx, dy, wait) {
     player.x += dx;
-    if (player.x < 0) {player.x = 0; return}
-    if (player.x > 12) {player.x = 12; return}
+    if (player.x < 0) { player.x = 0; return }
+    if (player.x > 12) { player.x = 12; return }
     player.y += dy;
-    if (player.y < 0) {player.y = 0; return}
-    if (player.y > 12) {player.y = 12; return}
+    if (player.y < 0) { player.y = 0; return }
+    if (player.y > 12) { player.y = 12; return }
 
     if (gameMap[player.x][player.y] === -99) {
         player.x -= dx;
@@ -159,17 +178,23 @@ function handleInput(dx, dy, wait) {
     }
 
     let mob;
-    mobs.forEach((m)=> {
+    mobs.forEach((m) => {
         if (m.x === player.x && m.y === player.y) mob = m;
     })
-    
+
     if (mob) {
-        tryBump(mob);
-        player.x -= dx;
-        player.y -= dy;
+        if (tryBump(mob)) {
+            player.x -= dx;
+            player.y -= dy;
+        }
     }
 
     moves++;
+    movesInRoom++;
+    if (movesInRoom > 13) {
+        movesInRoom = 0;
+        spawnMob();
+    }
 
     if (player.x === 6 && player.y === 0) {
         nextRoom();
@@ -181,11 +206,12 @@ function handleInput(dx, dy, wait) {
         gameMap[player.x][player.y] = 0;
         setTimeout(() => playerElement.style.transform = "scale(0.8)", 100);
     }
-    
-    
 
-    if (player.score < 0 || player.score >= 13) {
-        gameOver();
+
+
+    if (gameRunning && (player.score < 0 || player.score >= 13)) {
+        player.score = player.score >= 13 ? 13 : player.score;
+        gameOver(player.score >= 13 ? "Unlucky 13" : "You died");
     }
 
     updateMobs();
@@ -195,6 +221,7 @@ function handleInput(dx, dy, wait) {
 }
 
 function updateMobs() {
+    if (!gameRunning) return
     mobs.forEach((mob) => {
         if (mob.dead) return
         let direction = DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
@@ -217,8 +244,9 @@ function updateMobs() {
             mob.score += Math.max(-1, Math.min(diff, gameMap[tx][ty]));
             gameMap[tx][ty] = 0;
         }
-        
+
         if (mob.score < 0) {
+            showMessage(`You defeated a number.`);
             mob.dead = true;
             mob.element.remove();
             mob.element.style.display = "none";
@@ -227,13 +255,32 @@ function updateMobs() {
 }
 
 function tryBump(mob) {
-    if (!mob) return
+    if (!mob || mob.dead) return false
     if (mob.score > player.score) {
         player.score--;
+        showMessage(`The number ${mob.score} hit you.`);
+
+        if (player.score < 0) {
+            gameOver(`You were killed by the number ${mob.score}`);
+        }
+        playerElement.style.transform = "scale(0.5)";
+        playerElement.style.backgroundColor = "#f00";
+        setTimeout(() => {
+            playerElement.style.transform = "scale(0.8)";
+            playerElement.style.backgroundColor = "#fff";
+        }, 50);
     }
     if (player.score > mob.score) {
         mob.score--;
+        showMessage(`You hit the number ${mob.score}.`);
+        mob.element.style.transform = "scale(0.5)";
+        mob.element.style.backgroundColor = "#f00";
+        setTimeout(() => {
+            mob.element.style.transform = "scale(0.8)";
+            mob.element.style.backgroundColor = "#fff";
+        }, 50);
     }
+    return true
 }
 
 function updateMap() {
@@ -251,43 +298,44 @@ function updateMap() {
             if (mob) continue;
             const value = gameMap[x][y];
             if (value >= 13 || value <= -13) {
-                getNeighbours(x,y).forEach(([xx, yy]) => {
+                getNeighbours(x, y).forEach(([xx, yy]) => {
                     if (gameMap[xx]?.[yy] === undefined || Math.random() >= 0.13) return
                     if (xx === player.x && yy === player.y) return
                     gameMap[xx][yy] = value;
                 });
-                gameMap[x][y] = Math.random() < 0.13 ? -99 : 0;
+                gameMap[x][y] = Math.random() < 0.13 ? -13 : Math.random() < 0.13 ? 13 : 0;
             }
             if (Math.random() < 0.13 && gameMap[x][y] > -99) {
-                gameMap[x][y]+= averageSignOfNeighbours(x,y);
+                gameMap[x][y] += averageSignOfNeighbours(x, y);
             }
         }
     }
 }
 
 function getNeighbours(x, y) {
-    return [[-1,0],[1,0],[0,-1],[-0,1]].map(([i, j]) => [x+i,y+j]);
+    return [[-1, 0], [1, 0], [0, -1], [-0, 1]].map(([i, j]) => [x + i, y + j]);
 }
 
 function averageSignOfNeighbours(x, y) {
     let sum = 0;
     let count = 0;
-    for ([i, j] of [[-1,0],[1,0],[0,-1],[-0,1]]) {
-        if (x+i === 6 && y+j === 0) continue
+    for ([i, j] of [[-1, 0], [1, 0], [0, -1], [-0, 1]]) {
+        if (x + i === 6 && y + j === 0) continue
         if (gameMap[x + i]?.[y + j]) {
-            sum += gameMap[x+i][y+j];
+            sum += gameMap[x + i][y + j];
             count++;
         }
     }
     sum /= count;
-    return sum? sum / Math.abs(sum) : 0;
+    return sum ? sum / Math.abs(sum) : 0;
 }
 
 function nextRoom() {
     if (room === 13) {
-        return nextLevel();  
+        return nextLevel();
     }
     room++;
+    showMessage(`You have entered Room ${room}`);
     setupGame();
 }
 
@@ -297,23 +345,32 @@ function nextLevel() {
         return
     }
     room = 1;
+    showMessage(`You have cleared level ${level}!`);
     level++;
     setupGame();
 }
 
 function winGame() {
+    gameRunning = false;
+
     gameOverText.textContent = "WIN! WIN! WIN!"
+    gameOverMessage.textContent = `You defeated the number 13 in ${moves} moves!`;
     gameOverBar.style.transform = "scaleY(1)";
     window.removeEventListener("keydown", onKeyDown);
     window.addEventListener("keydown", onMenuKeyDown);
 }
 
-function gameOver() {
+function gameOver(str) {
+    gameRunning = false;
+
     gameOverText.textContent = "GAME OVER"
+
+    showMessage(str + " on level " + level + ".");
     gameOverBar.style.transform = "scaleY(1)";
+    gameOverMessage.textContent = str;
     window.removeEventListener("keydown", onKeyDown);
     window.addEventListener("keydown", onMenuKeyDown);
-}  
+}
 
 function onMenuKeyDown(event) {
     if (event.key === " ") {
@@ -323,24 +380,24 @@ function onMenuKeyDown(event) {
 }
 
 function restart() {
-        window.removeEventListener("keydown", onMenuKeyDown);
-        createPlayer();
-        room = 1;
-        level = 1;
-        moves = 0;
-        setupGame();
-    
-    
+    window.removeEventListener("keydown", onMenuKeyDown);
+    createPlayer();
+    room = 1;
+    level = 1;
+    moves = 0;
+    setupGame();
+
+
 
 }
 
 function onKeyDown(event) {
     if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "].includes(event.key)) {
-        const {key, code} = event;
+        const { key, code } = event;
         const dy = (key === "ArrowDown") - (key === "ArrowUp");
         const dx = (key === "ArrowRight") - (key === "ArrowLeft");
         const wait = (key === " ");
-        handleInput(dx, dy, wait);       
+        handleInput(dx, dy, wait);
         event.preventDefault();
     }
 }
@@ -350,10 +407,7 @@ function showMessage(str) {
     message.classList.add("message");
     message.textContent = str;
     messages.insertBefore(message, messages.firstChild);
-}
-
-for (let i = 0; i < 10; i++) {
-    showMessage("a much much much much much longer test message " + i);
+    message.scrollIntoView();
 }
 
 
